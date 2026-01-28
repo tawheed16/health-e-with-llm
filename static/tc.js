9212e59377fd7e1b1700a988f703327ee45c0f66
Mithilan1 <mithilan.thanabalasingam@dcmail.ca>
2026-01-28 17:10:46 -0500

    New

--- static/tc.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("closeBtn").addEventListener("click", () => {
-  // Tell parent (form.html) to close the modal overlay
-  if (window.parent) {
-    window.parent.postMessage("closeTC", "*");
-  } else {
-    window.close();
-  }
-});
